120cf891306c1d85ed4df1368b4d5c45bbaebd15
Afibride <125438145+Afibride@users.noreply.github.com>
2025-09-28 15:54:20 +0100

    Update api.js

--- language-frontend/src/api/api.js
+++ language-frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // 👇 Set your Laravel API URL (use environment variables for production!)
 const api = axios.create({
-  baseURL: 'https://talktribe-1-v86k.onrender.com',
+  baseURL: 'https://talktribe-bh45.onrender.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
